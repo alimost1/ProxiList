cf4ae41c85bdca2c2d5da30672bbc7eabcd80c29
alimost <alimost1@gmail.com>
2026-05-03 21:59:50 +0100

    feat: configure Next.js standalone output and add Docker orchestration files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",   // enables the tiny standalone server for Docker
 };
 
 module.exports = nextConfig;
